ffb82ed7333cda2bcce7f0d2b4dd783f30847295
roxanaldwg <roxana_ludwig@yahoo.de>
2022-02-27 18:47:24 +0100

    Updates

---
+++ precache-manifest.d339fd4f62af27121a2fb69bc7729a90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ce0b2b37bd6c8b18653",
+    "url": "/meet/static/css/main.19ced6f7.chunk.css"
+  },
+  {
+    "revision": "7ce0b2b37bd6c8b18653",
+    "url": "/meet/static/js/main.7ce0b2b3.chunk.js"
+  },
+  {
+    "revision": "efa4542dab26d995e301",
+    "url": "/meet/static/js/1.efa4542d.chunk.js"
+  },
+  {
+    "revision": "b75323fd636df612d81b",
+    "url": "/meet/static/js/2.b75323fd.chunk.js"
+  },
+  {
+    "revision": "04ed86561d53118d2796",
+    "url": "/meet/static/js/runtime~main.04ed8656.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/meet/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "81b53580ee30a47cfc3b66ae44e1ff17",
+    "url": "/meet/index.html"
+  }
+];
