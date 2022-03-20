ad2f6893b055815db17b974ad29234f0cfd463d7
roxanaldwg <roxana_ludwig@yahoo.de>
2022-03-20 20:52:35 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
 
   updateEvents = (location, eventCount = this.state.eventCount) => {
     getEvents().then((events) => {
-      const locationEvents = (location === "all" ? events : events.filter((event) => event.location === location));
+      let locationEvents = (location === "all" ? events : events.filter((event) => event.location === location));
       locationEvents = locationEvents.slice(0, eventCount)
       this.setState({
         events: locationEvents,
